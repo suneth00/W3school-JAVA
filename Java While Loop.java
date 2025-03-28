5c74547f2bfb47b619dce3e285b265729ae72cb0
suneth00 <156991073+suneth00@users.noreply.github.com>
2025-03-28 23:14:54 +0530

    Create Java While Loop.java
    w3school Java While Loop

---
+++ Java While Loop.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    int dice = 1;
+
+    while (dice <= 6) {
+      if (dice < 6) {
+        System.out.println("No Yatzy.");
+      } else {
+        System.out.println("Yatzy!");
+      }
+      dice = dice + 1;
+    }
+  }
+}
+
